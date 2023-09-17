dd48873a9e08b70e51175d6923ad8de9f574c821
Madan <madan.patakota@gmail.com>
2023-09-17 15:48:45 +0530

    basicc deployent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { EventsComponent } from './enduser/events/events.component';
-import { CoursesComponent } from './enduser/courses/courses.component';
-import { CourseDetailsComponent } from './enduser/course-details/course-details.component';
-import { TrainersComponent } from './enduser/trainers/trainers.component';
-import { AboutComponent } from './core/about/about.component';
-import { ArticlesComponent } from './enduser/articles/articles.component';
-import { HomeComponent } from './shared/components/home/home.component';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 //import { Swiper,SwiperOptions } from 'swiper';
